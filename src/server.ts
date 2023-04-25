add1e32e0a06026ff38559191baf8e138e58fc16
andrew boldrin <andrewboldrin@hotmail.com>
2023-04-25 18:35:38 -0300

    configurando eslint

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import { env } from "./env";
-import { app } from "./app";
+import { env } from './env'
+import { app } from './app'
 
-app.listen({
+app
+  .listen({
     host: '0.0.0.0', // para não ter problemas com a conexao com o front-end
-    port: env.PORT
-})
-.then(() => {
+    port: env.PORT,
+  })
+  .then(() => {
     console.log('HTTP Server Running!')
-})
+  })
