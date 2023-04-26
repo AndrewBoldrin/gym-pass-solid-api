b137faa0f49f81a42caf6bc491fbe0a9e37a63b8
andrew boldrin <andrewboldrin@hotmail.com>
2023-04-25 23:08:52 -0300

    tipando repositorio e criando metodo para verificar email

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,4 @@
-import { prisma } from '@/lib/prisma'
+import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseParams {
@@ -11,16 +11,12 @@ interface RegisterUseCaseParams {
 
 export class RegisterUseCase {
   // quando coloca o private ele ja vira uma variavel da classe
-  constructor(private usersRepository: any) {}
+  constructor(private usersRepository: UsersRepository) {}
 
   async execute({ name, email, password }: RegisterUseCaseParams) {
     const password_hash = await hash(password, 6)
 
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
       throw new Error('E-mail já existe.')
