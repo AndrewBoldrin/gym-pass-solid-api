4771e14df1aff36e5ea68d64503ef3fce9339f65
andrew boldrin <andrewboldrin@hotmail.com>
2023-04-26 10:09:34 -0300

    criando testes unitarios usando in memory database

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,11 +1,16 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@prisma/client'
 
 interface RegisterUseCaseParams {
   name: string
   email: string
   password: string
+}
+
+interface RegisterUseCaseResponse {
+  user: User
 }
 
 // ao inves de a classe instanciar as dependencias new Prism..., ela recebe as dependencias
@@ -14,7 +19,11 @@ export class RegisterUseCase {
   // quando coloca o private ele ja vira uma variavel da classe
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseParams) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseParams): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -23,6 +32,12 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({ name, email, password_hash })
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+
+    return { user }
   }
 }
