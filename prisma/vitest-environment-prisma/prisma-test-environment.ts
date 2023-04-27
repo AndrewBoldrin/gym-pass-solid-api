1dcdc7a93798040b74f0b5659d1b5f95a4d5c4f9
andrew boldrin <andrewboldrin@hotmail.com>
2023-04-27 15:58:54 -0300

    direcionando o ambiente de teste e usando ambiente de teste criado como lib para o projeto entender

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -4,11 +4,13 @@ export default <Environment>{
   name: 'prisma',
   async setup() {
     // codigo que vai executar antes dos test (antes de cada arquivo de test)
-    console.log('')
+    console.log('Setup')
 
     return {
       // metodo a ser executado apos cada arquivo de test ser executado
-      teardown() {},
+      async teardown() {
+        console.log('Teardown')
+      },
     }
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    // 'src/http/controllers/**' - caminho da pasta em que queremos executar o ambiente de teste criado pelo vitest
+    // 'prisma' - tem que ser o mesmo nome criado da pasta prisma/vitest-environment-prisma (nome depois do vitest-environmennt-{nome})
+    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+  },
 })
