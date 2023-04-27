51160a8779aa40144d3d839fc2453886a7d331ae
andrew boldrin <andrewboldrin@hotmail.com>
2023-04-27 15:44:27 -0300

    setando ambiente de test prisma

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,14 @@
+import { Environment } from 'vitest'
+
+export default <Environment>{
+  name: 'prisma',
+  async setup() {
+    // codigo que vai executar antes dos test (antes de cada arquivo de test)
+    console.log('')
+
+    return {
+      // metodo a ser executado apos cada arquivo de test ser executado
+      teardown() {},
+    }
+  },
+}
