6b1727a06889b28096a8e7e1b32931b7845e0607
andrew boldrin <andrewboldrin@hotmail.com>
2023-04-27 10:55:15 -0300

    caso de uso fetch checkin history + test

---
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,22 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface FetchUserCheckInHistoryUseCaseRequest {
+  userId: string
+}
+
+interface FetchUserCheckInHistoryUseCaseResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInHistoryUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+  }: FetchUserCheckInHistoryUseCaseRequest): Promise<FetchUserCheckInHistoryUseCaseResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(userId)
+
+    return { checkIns }
+  }
+}
